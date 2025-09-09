75f75bc24f691bdeee411c76659aa6b5c016f71d
Faiyaz Azam <faiyazazam00@gmail.com>
2025-09-09 12:35:51 -0400

    update 2

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Faiyaz',
+    author_email= 'faiyazazam00@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
